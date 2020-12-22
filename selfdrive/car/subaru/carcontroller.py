2c37fa4338a4e37d78ba5378da7f42477acb4d85
letsdudiss <t.pham@alerton.com.au>
2020-12-22 13:45:54 +1100

    Add Throttle signal intercept and bypass for Pre Global platform

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -84,63 +84,79 @@ class CarController():
       self.apply_steer_last = apply_steer
 
     #----------------------Subaru STOP AND GO------------------------
-    #Car can only be in HOLD state (3) if it is standing still
-    # => if not in HOLD state car has to be moving or driver has taken action
-    if CS.cruise_state != 3:
-      self.sng_throttle_tap_cnt = 0           #Reset throttle tap message count when car starts moving
-      self.sng_resume_acc = False             #Cancel throttle tap when car starts moving
-      self.sng_has_recorded_distance = False  #Reset has_recorded_distance flag once car started moving
+    if CS.CP.carFingerprint in PREGLOBAL_CARS: 
+      #PRE-GLOBAL
+      throttle_cmd = -1 #just forward throttle message for now
+      #-------------Send throttle message-------------
+      if self.throttle_cnt != CS.throttle_msg["Counter"]:
+        can_sends.append(subarucan.create_preglobal_throttle_control(self.packer, CS.throttle_msg, throttle_cmd))
+        self.throttle_cnt = CS.throttle_msg["Counter"]
+      #------------------------------------------------  
+    else:
+      #Global
+      #Car can only be in HOLD state (3) if it is standing still
+      # => if not in HOLD state car has to be moving or driver has taken action
+      if CS.cruise_state != 3:
+        self.sng_throttle_tap_cnt = 0           #Reset throttle tap message count when car starts moving
+        self.sng_resume_acc = False             #Cancel throttle tap when car starts moving
+        self.sng_has_recorded_distance = False  #Reset has_recorded_distance flag once car started moving
 
-    #Reset SNG distance threshold to limit value if we havent recorded a reference distance threshold
-    #This is to make sure car will always move forward when lead car moves before SnG reference distance
-    #threshold is recorded
-    if not self.sng_has_recorded_distance:
-      self.sng_distance_threshold = self.params.SNG_DISTANCE_LIMIT
-
-    #Record the time at which CruiseState change to HOLD (3)
-    if self.prev_cruise_state != 3 and CS.cruise_state == 3:
-      self.cruise_state_change_time = time.time_ns()
-
-    #While in HOLD, wait <ES_CLOSE_DISTANCE_SETTLE_TIME> nanoseconds (since Cruise state changes to HOLD)
-    #before recording SnG lead car reference distance
-    if (enabled
-        and CS.cruise_state == 3                #in HOLD state
-        and not self.sng_has_recorded_distance  #has not recorded reference distance
-        and time.time_ns() > self.cruise_state_change_time + self.params.ES_CLOSE_DISTANCE_SETTLE_TIME): #wait 200ms before recording reference distance
-      self.sng_distance_threshold = CS.close_distance
-      self.sng_has_recorded_distance = True     #Set flag to true so sng_distance_threshold wont be recorded again until car moves
-      #Limit lead car reference distance to <SNG_DISTANCE_LIMIT>
-      if self.sng_distance_threshold > self.params.SNG_DISTANCE_LIMIT:
+      #Reset SNG distance threshold to limit value if we havent recorded a reference distance threshold
+      #This is to make sure car will always move forward when lead car moves before SnG reference distance
+      #threshold is recorded
+      if not self.sng_has_recorded_distance:
         self.sng_distance_threshold = self.params.SNG_DISTANCE_LIMIT
 
-    #Trigger THROTTLE TAP when in hold and close_distance increases > SNG distance threshold (with deadband)
-    #false positives caused by pedestrians/cyclists crossing the street in front of car
-    self.sng_resume_acc = False
-    if (enabled
-        and CS.cruise_state == 3 #cruise state == 3 => ACC HOLD state
-        and CS.close_distance > self.sng_distance_threshold + self.params.SNG_DISTANCE_DEADBAND #lead car distance is within SnG operating range
-        and CS.close_distance < 255
-        and CS.car_follow == 1):
-      self.sng_resume_acc = True
+      #Record the time at which CruiseState change to HOLD (3)
+      if self.prev_cruise_state != 3 and CS.cruise_state == 3:
+        self.cruise_state_change_time = time.time_ns()
 
-    #Send a throttle tap to resume ACC
-    throttle_cmd = -1 #normally, just forward throttle msg from ECU
-    if self.sng_resume_acc:
-      #Send Maximum <THROTTLE_TAP_LIMIT> to get car out of HOLD
-      if self.sng_throttle_tap_cnt < self.params.THROTTLE_TAP_LIMIT:
-        throttle_cmd = self.params.THROTTLE_TAP_LEVEL
-        self.sng_throttle_tap_cnt += 1
-      else:
-        self.sng_throttle_tap_cnt = -1
-        self.sng_resume_acc = False
-    #TODO: Send cruise throttle to get car up to speed. There is a 2-3 seconds delay after
-    # throttle tap is sent and car start moving. EDIT: This is standard with Toyota OP's SnG
-    #pseudo: !!!WARNING!!! Dangerous, proceed with CARE
-    #if sng_resume_acc is True && has been 1 second since sng_resume_acc turns to True && current ES_Throttle < 2000
-    #    send ES_Throttle = 2000
+      #While in HOLD, wait <ES_CLOSE_DISTANCE_SETTLE_TIME> nanoseconds (since Cruise state changes to HOLD)
+      #before recording SnG lead car reference distance
+      if (enabled
+          and CS.cruise_state == 3                #in HOLD state
+          and not self.sng_has_recorded_distance  #has not recorded reference distance
+          and time.time_ns() > self.cruise_state_change_time + self.params.ES_CLOSE_DISTANCE_SETTLE_TIME): #wait 200ms before recording reference distance
+        self.sng_distance_threshold = CS.close_distance
+        self.sng_has_recorded_distance = True     #Set flag to true so sng_distance_threshold wont be recorded again until car moves
+        #Limit lead car reference distance to <SNG_DISTANCE_LIMIT>
+        if self.sng_distance_threshold > self.params.SNG_DISTANCE_LIMIT:
+          self.sng_distance_threshold = self.params.SNG_DISTANCE_LIMIT
 
-    #Update prev values
-    self.prev_cruise_state = CS.cruise_state
+      #Trigger THROTTLE TAP when in hold and close_distance increases > SNG distance threshold (with deadband)
+      #false positives caused by pedestrians/cyclists crossing the street in front of car
+      self.sng_resume_acc = False
+      if (enabled
+          and CS.cruise_state == 3 #cruise state == 3 => ACC HOLD state
+          and CS.close_distance > self.sng_distance_threshold + self.params.SNG_DISTANCE_DEADBAND #lead car distance is within SnG operating range
+          and CS.close_distance < 255
+          and CS.car_follow == 1):
+        self.sng_resume_acc = True
+
+      #Send a throttle tap to resume ACC
+      throttle_cmd = -1 #normally, just forward throttle msg from ECU
+      if self.sng_resume_acc:
+        #Send Maximum <THROTTLE_TAP_LIMIT> to get car out of HOLD
+        if self.sng_throttle_tap_cnt < self.params.THROTTLE_TAP_LIMIT:
+          throttle_cmd = self.params.THROTTLE_TAP_LEVEL
+          self.sng_throttle_tap_cnt += 1
+        else:
+          self.sng_throttle_tap_cnt = -1
+          self.sng_resume_acc = False
+      #TODO: Send cruise throttle to get car up to speed. There is a 2-3 seconds delay after
+      # throttle tap is sent and car start moving. EDIT: This is standard with Toyota OP's SnG
+      #pseudo: !!!WARNING!!! Dangerous, proceed with CARE
+      #if sng_resume_acc is True && has been 1 second since sng_resume_acc turns to True && current ES_Throttle < 2000
+      #    send ES_Throttle = 2000
+
+      #Update prev values
+      self.prev_cruise_state = CS.cruise_state
+
+      #-------------Send throttle message-------------
+      if self.throttle_cnt != CS.throttle_msg["Counter"]:
+        can_sends.append(subarucan.create_throttle(self.packer, CS.throttle_msg, throttle_cmd))
+        self.throttle_cnt = CS.throttle_msg["Counter"]
+      #------------------------------------------------  
     #------------------------------------------------------------------
 
     # *** alerts and pcm cancel ***
@@ -176,18 +192,12 @@ class CarController():
 
       #If Auto Stop Start has gone to state 3 at least once, it means either we have successfully turn off autoStopStart
       #or driver manually turn it off before we got to it
-      if CS.autoStopStartDisabled:
-        self.has_set_auto_ss = True
+      #if CS.autoStopStartDisabled:
+      #  self.has_set_auto_ss = True
 
       #Send message to press AutoSS button, only do it once, when car starts up, after that, driver can turn it back on if they want
-      if self.dashlights_cnt != CS.dashlights_msg["Counter"] and not self.has_set_auto_ss and self.feature_no_engine_stop_start:
-        can_sends.append(subarucan.create_dashlights(self.packer, CS.dashlights_msg, True))
-        self.dashlights_cnt = CS.dashlights_msg["Counter"]
-
-      #-------------Subaru STOP AND GO: Send throttle message-------------
-      if self.throttle_cnt != CS.throttle_msg["Counter"]:
-        can_sends.append(subarucan.create_throttle(self.packer, CS.throttle_msg, throttle_cmd))
-        self.throttle_cnt = CS.throttle_msg["Counter"]
-      #-------------------------------------------------------------------  
+      #if self.dashlights_cnt != CS.dashlights_msg["Counter"] and not self.has_set_auto_ss and self.feature_no_engine_stop_start:
+      #  can_sends.append(subarucan.create_dashlights(self.packer, CS.dashlights_msg, True))
+      #  self.dashlights_cnt = CS.dashlights_msg["Counter"]
 
     return can_sends
